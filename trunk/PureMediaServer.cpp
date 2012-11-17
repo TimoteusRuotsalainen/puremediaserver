77aa706454ce21f717c7f07db9587d24043c7132
belfegor@gmail.com <belfegor@gmail.com@b788cefc-50ff-11de-b5d5-e7cfa91e8af1>
2012-11-17 20:45:46 +0000

    - Versión 0.0.5! - Pequeñas correciones

--- trunk/PureMediaServer.cpp
+++ trunk/PureMediaServer.cpp
@@ -245,7 +245,7 @@ void PureMediaServer::pdstart()
     qDebug()<<"error listening tcpServer";
     }
     // Arrancamos el proceso Pure Data
-    pd->start("pd-gem", QStringList()<< "-stderr" << "pms-video.pd");
+    pd->start("pd-gem", QStringList()<< "-nogui" << "-stderr" << "pms-video.pd");
     connect(pd, SIGNAL(readyReadStandardError()), this, SLOT(stdout()));
     open();
 }
@@ -390,7 +390,7 @@ void PureMediaServer::stdout() {
     if (!out.isEmpty())
     {
         qDebug() << out;
-        ui.textEdit->appendPlainText(out);
+//        ui.textEdit->appendPlainText(out);
     }
 }
 
