af7e09740fd92185ed559bfb59dc0ab49038d701
belfegor <belfegor@gmail.com>
2012-11-17 20:45:46 +0000

    - Versión 0.0.5! - Pequeñas correciones

--- scripts/install_wheezy.sh
+++ scripts/install_wheezy.sh
@@ -1,9 +1,21 @@
+# Comprobamos que el repositorio no está
+# Hasta que no haya repositorio...
+#cat /etc/apt/sources.list | while read fileline
+#do
+#if ["$fileline" == "deb http://apt.openlighting.org/debian/ wheezy main"]; then
+#	exit		
+#fi
+#done
+
 # Añadimos el repositorio ola
-echo "deb http://apt.openlighting.org/debian/ wheeze main" >> /etc/apt/sources.list
+#echo "deb http://apt.openlighting.org/debian/ wheezy main" >> /etc/apt/sources.list
+
+# Instalamos el paquete suministrado con el release
+dpkg -i ola_0.8.26-1_i386.deb
 
 # Install Pure Data y OLA
 # libav para generar thumbs
-sudo apt-get update
-sudo apt-get install ola puredata gem tcl tk pd-zexy pd-cyclone pd-hcs libav-tools
+apt-get update
+apt-get install ola puredata gem tcl tk pd-zexy pd-cyclone pd-hcs libav-tools
 
 
